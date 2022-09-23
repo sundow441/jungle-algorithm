b3a87362666caab880c1e68e8f4efceb57a5654f
kimseungdeok <tmdejr1117@gmail.com>
2022-09-24 03:27:26 +0900

    resolves : 풀이 완료 코드

--- Kim-seungdeok/2562.py
+++ Kim-seungdeok/2562.py
@@ -2,11 +2,7 @@ import sys
 
 sys.stdin = open('example.txt', 'r')
 
-input = sys.stdin.read().split('\n')
-
-input = list(map(int, input))
-
-print(input)
+input = list(map(int, sys.stdin.read().splitlines()))
 
 print(max(input))
 print(input.index(max(input))+1)
